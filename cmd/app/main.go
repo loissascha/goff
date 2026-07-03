db7a2848e19e65b319131296e7c59d053ab541de
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-07-03 11:33:16 +0200

    start http server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
 func main() {
+	addr := env("ADDR", ":8118")
+
+	server := &http.Server{
+		Addr:              addr,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	log.Printf("listening on %s", addr)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+func env(key string, fallback string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		return fallback
+	}
+	return v
 }
